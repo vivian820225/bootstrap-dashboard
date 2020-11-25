b31ddbe834298dcde5a6a9043c863796ba9dc90d
unknown <vivian820225@gmail.com>
2020-11-25 17:02:30 +0800

    fix slideout header

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,9 +8,37 @@ $(function() {
   });
 
   // Toggle button
-  document.querySelector('#toggleBtn').addEventListener('click', function() {
+  var fixedHeader = document.getElementById('stickyHeader');
+  var toggleBtn = document.querySelector('#toggleBtn');
+  toggleBtn.addEventListener('click', function() {
     this.classList.toggle('rotate');
     slideout.toggle();
+  });
+
+  if (!slideout.isOpen()) {
+    toggleBtn.classList.remove('rotate');
+  }
+
+  slideout.on('translate', function(translated) {
+    fixedHeader.style.transform = 'translateX(' + translated + 'px)';
+  });
+  
+  slideout.on('beforeopen', function () {
+    fixedHeader.style.transition = 'transform 300ms ease';
+    fixedHeader.style.transform = 'translateX(248px)';
+  });
+  
+  slideout.on('beforeclose', function () {
+    fixedHeader.style.transition = 'transform 300ms ease';
+    fixedHeader.style.transform = 'translateX(0px)';
+  });
+  
+  slideout.on('open', function () {
+    fixedHeader.style.transition = '';
+  });
+  
+  slideout.on('close', function () {
+    fixedHeader.style.transition = '';
   });
 
   // 次選單收合
